0c701d6b9ca3f16f67f4a6daef78f2945347d6aa
blitz137 <43609419+blitz137@users.noreply.github.com>
2019-04-09 16:50:14 -0500

    'commit'

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,42 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
+
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
 
-const Layout = ({ children }) => (
+
+export default ({ children }) => (
   <StaticQuery
     query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+      query {
+        allContentfulHomePage {
+    edges {
+      node {
+        title
+        date
+        content {
+          content
+        }
+        image {
+          file {
+            url
+            fileName
+            contentType
           }
         }
       }
-    `}
+    }
+  }
+      }
+    `
+}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+      <div
+       
+      >
+      <h3> {data.site.siteMetadata.title}</h3>
+        
+        {children}
+      </div>
     )}
   />
 )
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
-
-export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,46 @@
-import React from "react"
-import { Link } from "gatsby"
-const IndexPage = () => (
-<div>
-  <p>Hello World!</p>
-  <p>still works</p>
-</div>
-)
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
 
-export default IndexPage
+
+const HomePage = () => (
+  <StaticQuery
+    query={graphql`
+      query HomePage {
+        
+        contentfulHomePage {
+          title
+          date
+          content {
+            content
+          }
+          image {
+            file {
+              url
+            }
+          }
+        }
+      }
+    `}
+    render={({
+        
+      contentfulHomePage: {
+        title,
+        date,
+        content: { content },
+        image: {
+          file: { url }
+        }
+      }
+    }) => (
+      <>
+        <h1>{title}</h1>
+        <small>Created on {date}</small>
+      
+        <img src={url} />
+        <p>{content}</p>
+      </>
+    )}
+  />
+);
+
+export default HomePage; 
