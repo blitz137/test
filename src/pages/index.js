6e72d0d41cc13f36ebd56402e6bc650a87abb522
blitz137 <43609419+blitz137@users.noreply.github.com>
2019-04-10 12:47:26 -0500

    'commit'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,14 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
 
+import Home from '../components/home'
 
-const HomePage = () => (
-  <StaticQuery
-    query={graphql`
-      query HomePage {
-        
-        contentfulHomePage {
-          title
-          date
-          content {
-            content
-          }
-          image {
-            file {
-              url
-            }
-          }
-        }
-      }
-    `}
-    render={({
-        
-      contentfulHomePage: {
-        title,
-        date,
-        content: { content },
-        image: {
-          file: { url }
-        }
-      }
-    }) => (
-      <>
-        <h1>{title}</h1>
-        <small>Created on {date}</small>
-      
-        <img src={url} />
-        <p>{content}</p>
-      </>
-    )}
-  />
-);
-
-export default HomePage; 
+export default () => (
+  <div>
+  <Home/>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </div>
+)
